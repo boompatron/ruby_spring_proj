b6310d60e31851194622386c987f506687d21e20
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-05-19 00:39:10 +0900

    220519_1_init

---
+++ Chapter01/src/main/java/com/rubypaper/Chapter01/Chapter01Application.java
@@ -0,0 +1,24 @@
+package com.rubypaper.Chapter01;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter01Application {
+
+	public static void main(String[] args) {
+		// run as java application
+		// no conflict even multiple run
+		// if WebApplicationType.SERVLET => run as web or undo
+		SpringApplication application = new SpringApplication(Chapter01Application.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.setBannerMode(Banner.Mode.OFF);	// Hide Spring Banner
+		application.run(args);
+
+		// run as web application
+		SpringApplication.run(Chapter01Application.class, args);
+	}
+
+}
